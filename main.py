5b4d3e6803f2d1da7fb4e2423f2c8147181ac45a
Oliver Gaskell <olivergaskell700@gmail.com>
2020-12-17 12:29:41 +0000

    Add searchbar

--- main.py
+++ main.py
@@ -28,11 +28,18 @@ class MainWindow(Gtk.Window):
             self.logo_pixbuf = self.logo_pixbuf.scale_simple(24, 24, GdkPixbuf.InterpType.BILINEAR)
             self.logo = Gtk.Image.new_from_pixbuf(self.logo_pixbuf)
 
+            self.search = Gtk.SearchBar()
+            self.search_entry = Gtk.SearchEntry()
+            self.search.connect_entry(self.search_entry)
+
             self.wm_buttons = WmButtons()
 
             self.pack_start(self.logo)
             self.pack_start(self.menu_btn)
+
             self.pack_end(self.wm_buttons)
+            self.pack_end(self.search_entry)
+            self.pack_end(self.search)
 
 
 win = MainWindow()
