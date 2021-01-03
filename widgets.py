2b24abcfc051b9453b4c89e01ecd84e6e272847a
Oliver Gaskell <olivergaskell700@gmail.com>
2021-01-03 16:49:41 +0000

    Add post widget

--- widgets.py
+++ widgets.py
@@ -36,3 +36,40 @@ class WmButtons(Gtk.Box):
             except ValueError:
                 print("error :(")
                 self.win.maximize()
+
+
+class Post(Gtk.Box):
+    def __init__(self, sub="r/test", user="u/test", title="Test Post"):
+        self.sub = sub
+        self.user = user
+        self.title = title
+
+        Gtk.Box.__init__(self, Gtk.Orientation.VERTICAL, 0)
+        self.set_orientation(Gtk.Orientation.VERTICAL)
+        Gtk.StyleContext.add_class(self.get_style_context(), "frame")
+        Gtk.StyleContext.add_class(self.get_style_context(), "post")
+        self.set_hexpand(True)
+
+        self.subuser_box = self.Post_header()
+        self.pack_start(self.subuser_box, True, True, 10)
+
+        self.post_title = Gtk.Label(label=self.title)
+        self.post_title.set_alignment(0, 0)
+        Gtk.StyleContext.add_class(self.post_title.get_style_context(), "post_title")
+        self.pack_start(self.post_title, True, True, 10)
+
+    class Post_header(Gtk.Box):
+        def __init__(self, sub="r/test", user="u/test"):
+            self.sub = sub
+            self.user = user
+
+            Gtk.Box.__init__(self, Gtk.Orientation.HORIZONTAL, 10)
+            Gtk.StyleContext.add_class(self.get_style_context(), "post_header")
+            self.set_hexpand(True)
+
+            self.sub_label = Gtk.Label(label=self.sub)
+            self.user_label = Gtk.Label(label=self.user)
+            Gtk.StyleContext.add_class(self.sub_label.get_style_context(), "post_sub")
+            Gtk.StyleContext.add_class(self.user_label.get_style_context(), "post_user")
+            self.pack_start(self.sub_label, False, False, 10)
+            self.pack_end(self.user_label, False, False, 10)
